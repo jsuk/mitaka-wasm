3a7a7a05aaa4eb9cd342911d4ff578a753352d28
jsuk <jeremy.suk@gmail.com>
2026-06-13 15:41:02 +0900

    Add Japan-region ad interstitial that gates app start (+ 1 more)
    - Add Japan-region ad interstitial that gates app start
    - Fingerprint data bundles + optional loader to fix progress overshoot

--- mitaka_optional-dcc24fe.js
+++ mitaka_optional-dcc24fe.js
@@ -18,8 +18,8 @@
         // web worker
         PACKAGE_PATH = encodeURIComponent(location.pathname.substring(0, location.pathname.lastIndexOf('/')) + '/');
       }
-      var PACKAGE_NAME = '/home/jsuk/git/mitaka-clone/build/mitaka_optional.data';
-      var REMOTE_PACKAGE_BASE = 'mitaka_optional.data';
+      var PACKAGE_NAME = '/home/jsuk/git/mitaka-clone/build/mitaka_optional-dcc24fe.data';
+      var REMOTE_PACKAGE_BASE = 'mitaka_optional-dcc24fe.data';
       var REMOTE_PACKAGE_NAME = Module['locateFile'] ? Module['locateFile'](REMOTE_PACKAGE_BASE, '') : REMOTE_PACKAGE_BASE;
       var REMOTE_PACKAGE_SIZE = metadata['remote_package_size'];
 
@@ -107,9 +107,9 @@ Module['FS_createPath']("/", "data", true, true);
         Module['FS_createDataFile'](name, null, data, true, true, true);
         Module['removeRunDependency'](`fp ${name}`);
           }
-          Module['removeRunDependency']('datafile_/home/jsuk/git/mitaka-clone/build/mitaka_optional.data');
+          Module['removeRunDependency']('datafile_/home/jsuk/git/mitaka-clone/build/mitaka_optional-dcc24fe.data');
       }
-      Module['addRunDependency']('datafile_/home/jsuk/git/mitaka-clone/build/mitaka_optional.data');
+      Module['addRunDependency']('datafile_/home/jsuk/git/mitaka-clone/build/mitaka_optional-dcc24fe.data');
 
       if (!Module['preloadResults']) Module['preloadResults'] = {};
 
