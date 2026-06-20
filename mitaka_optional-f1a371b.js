c6af37b981088d6bba52a84166b812d0a3b22e5a
jsuk <jeremy.suk@gmail.com>
2026-06-20 13:19:29 +0900

    Use portrait Rakuten banner on mobile, corner the QR code

--- mitaka_optional-f1a371b.js
+++ mitaka_optional-f1a371b.js
@@ -18,8 +18,8 @@
         // web worker
         PACKAGE_PATH = encodeURIComponent(location.pathname.substring(0, location.pathname.lastIndexOf('/')) + '/');
       }
-      var PACKAGE_NAME = '/home/jsuk/git/mitaka-clone/build/mitaka_optional-c77f184.data';
-      var REMOTE_PACKAGE_BASE = 'mitaka_optional-c77f184.data';
+      var PACKAGE_NAME = '/home/jsuk/git/mitaka-clone/build/mitaka_optional-f1a371b.data';
+      var REMOTE_PACKAGE_BASE = 'mitaka_optional-f1a371b.data';
       var REMOTE_PACKAGE_NAME = Module['locateFile'] ? Module['locateFile'](REMOTE_PACKAGE_BASE, '') : REMOTE_PACKAGE_BASE;
       var REMOTE_PACKAGE_SIZE = metadata['remote_package_size'];
 
@@ -107,9 +107,9 @@ Module['FS_createPath']("/", "data", true, true);
         Module['FS_createDataFile'](name, null, data, true, true, true);
         Module['removeRunDependency'](`fp ${name}`);
           }
-          Module['removeRunDependency']('datafile_/home/jsuk/git/mitaka-clone/build/mitaka_optional-c77f184.data');
+          Module['removeRunDependency']('datafile_/home/jsuk/git/mitaka-clone/build/mitaka_optional-f1a371b.data');
       }
-      Module['addRunDependency']('datafile_/home/jsuk/git/mitaka-clone/build/mitaka_optional-c77f184.data');
+      Module['addRunDependency']('datafile_/home/jsuk/git/mitaka-clone/build/mitaka_optional-f1a371b.data');
 
       if (!Module['preloadResults']) Module['preloadResults'] = {};
 
